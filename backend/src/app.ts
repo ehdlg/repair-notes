9a6e79fe7e984ff75180c6b405b852d6dd5a7db5
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-06-27 09:53:33 +0200

    refactor(app): add delivery notes route to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,8 @@
-import express, { Request, Response } from 'express';
-import DeliveryNote from './models';
+import express from 'express';
 import { initDb } from './db';
-import { CreationAttributes } from 'sequelize';
-import { IDeliveryNote } from './types';
+import deliveryNoteRoute from './routes/';
 import { errorHandler, notFound } from './middlewares';
-import { validation, getRules } from './middlewares/validation';
 import 'dotenv/config';
-import { DEFAULT_LIMIT, DEFAULT_OFFSET } from './constants';
 
 const app = express();
 const { PORT } = process.env;
@@ -23,25 +19,7 @@ async function main() {
 
 app.use(express.json());
 
-app.get('/', getRules, validation, async (req: Request, res: Response) => {
-  const limit = req.validatedData?.limit || DEFAULT_LIMIT;
-  const offset = req.validatedData?.offset || DEFAULT_OFFSET;
-
-  const notes = await DeliveryNote.getAll({ limit, offset });
-
-  return res.json(notes);
-});
-
-app.post('/', async (req, res, next) => {
-  try {
-    const newNote: CreationAttributes<IDeliveryNote> = req.body;
-    const createdNote = await DeliveryNote.create(newNote);
-
-    return res.json(createdNote);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use('/api', deliveryNoteRoute);
 
 app.use(notFound);
 
