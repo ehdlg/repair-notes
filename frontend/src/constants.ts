f1d0da2fa8e4c3e4968f38c2728ccb279bf46b3a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-06-28 17:27:17 +0200

    feat(constants): add navbar elements constant

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,16 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+
+export const NAVBAR_ELEMENTS: { url: string; name: string }[] = [
+  {
+    url: '/notas',
+    name: 'Notas de reparación',
+  },
+  {
+    url: '/nueva',
+    name: 'Crear nueva nota',
+  },
+  {
+    url: '/pendientes',
+    name: 'Notas de reparación pendientes',
+  },
+];
