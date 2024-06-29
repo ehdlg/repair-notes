92c061df21af68dc46a0f4f84addfdcdf5a34911
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-06-29 09:45:56 +0200

    feat(components): add Loading component, add Loading styles and animation

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='loader ease-linear rounded-full border-4 border-t-4 border-slate-600 h-12 w-12'></div>
+    </div>
+  );
+}
+
+export default Loading;
