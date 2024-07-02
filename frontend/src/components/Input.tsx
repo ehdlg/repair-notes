bd73ab0eeaa4b1d0406e911fc3658e3070a9ebfe
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-02 13:00:56 +0200

    feat(components): update Input to use useId and receive register prop

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,23 +1,37 @@
+import { useId } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import { InputsType } from '../types';
+import { formatDateToInput } from '../utils';
 function Input({
   label,
   name,
   type,
   defaultValue,
-}: {
-  label: string;
-  name: string;
-  type: string;
-  defaultValue: string | number | undefined;
+  register,
+  disabled = false,
+}: InputsType & {
+  register?: UseFormRegisterReturn;
+  disabled?: boolean;
 }) {
+  const id = useId();
   return (
-    <label htmlFor='client' className='text-base text-gray-500 font-semibold '>
+    <label htmlFor={id} className='text-base text-gray-500 font-semibold'>
       {label}
       <input
         type={type}
         name={name}
-        id={name}
-        defaultValue={defaultValue}
-        className='block font-normal text-gray-900 bg-gray-50 border w-3/4 border-gray-200 p-1 rounded-md'
+        id={id}
+        defaultValue={
+          type != 'date'
+            ? defaultValue?.toString()
+            : defaultValue != null
+            ? formatDateToInput(defaultValue as Date)
+            : ''
+        }
+        defaultChecked={type == 'checkbox' && Boolean(defaultValue)}
+        className='block font-normal text-gray-700 bg-gray-50 border w-3/4 border-gray-200 p-1 rounded-md disabled:cursor-not-allowed disabled:opacity-50'
+        disabled={disabled}
+        {...register}
       />
     </label>
   );
