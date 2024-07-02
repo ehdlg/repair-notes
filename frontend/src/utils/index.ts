cd8edbdf5374fbdd6239983ae95471af7ec4aba4
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-02 13:01:17 +0200

    feat(utils): add formatToDate function

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -9,3 +9,10 @@ export function calculatePagination(page: number, size: number) {
 
   return { limit, offset };
 }
+
+export function formatDateToInput(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
