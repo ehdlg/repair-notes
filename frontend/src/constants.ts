863e693b5c2a58a37ca4ba747ab271c77892084e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-03 18:31:03 +0200

    feat(constants): update input validation and error description

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -24,29 +24,35 @@ export const DEFAULT_SIZE = 50;
 export const CREATE_INPUTS: InputsType[] = [
   {
     label: 'Cliente',
-    defaultValue: '',
     name: 'client',
     type: 'text',
     options: {
-      required: false,
+      required: {
+        value: true,
+        message: 'El nombre del cliente es obligatorio',
+      },
     },
   },
   {
     label: 'Máquina/Modelo',
-    defaultValue: '',
     name: 'model',
     type: 'text',
     options: {
-      required: false,
+      required: {
+        value: true,
+        message: 'El nombre de la máquina es obligatorio',
+      },
     },
   },
   {
     label: 'Avería',
-    defaultValue: '',
     name: 'malfunction',
     type: 'text',
     options: {
-      required: false,
+      required: {
+        value: true,
+        message: 'La avería de la máquina es obligatoria',
+      },
     },
   },
   {
@@ -54,7 +60,11 @@ export const CREATE_INPUTS: InputsType[] = [
     name: 'phoneNumber',
     type: 'text',
     options: {
-      required: false,
+      required: {
+        value: true,
+        message: 'El número de teléfono del cliente es obligatorio',
+      },
+      pattern: { value: /^[0-9]{9}$/, message: 'El número de teléfono debe tener 9 dígitos' },
     },
   },
   {
@@ -63,13 +73,16 @@ export const CREATE_INPUTS: InputsType[] = [
     name: 'entryDate',
     type: 'date',
     options: {
-      required: false,
+      required: {
+        value: true,
+        message: 'La fecha de entrada es obligatoria',
+      },
+      valueAsDate: true,
     },
   },
 
   {
     label: 'Garantía',
-    defaultValue: '',
     name: 'garanty',
     type: 'checkbox',
     options: {
@@ -78,9 +91,7 @@ export const CREATE_INPUTS: InputsType[] = [
   },
 ] as const;
 
-export const EDIT_INPUTS: InputsType[] = CREATE_INPUTS.map((input) => {
-  return { ...input, options: { required: false } };
-}).concat([
+export const EDIT_INPUTS: InputsType[] = CREATE_INPUTS.concat([
   {
     label: 'Fecha de salida',
     name: 'departureDate',
