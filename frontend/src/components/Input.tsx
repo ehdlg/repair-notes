a47d844c2b0967f04498dfb35c0e474ed94c2816
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-04 10:24:20 +0200

    feat(styles): update focus input styles

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -20,7 +20,7 @@ function Input({
         type={type}
         name={name}
         id={id}
-        className='block font-normal text-gray-700 bg-gray-50 border w-3/4 border-gray-200 p-1 rounded-md disabled:cursor-not-allowed disabled:opacity-50'
+        className='block font-normal text-gray-700 bg-gray-50 border w-3/4 border-gray-200 focus:border-gray-400 focus:border-2 p-1 rounded-md outline-none disabled:cursor-not-allowed disabled:opacity-50'
         disabled={disabled}
         {...register}
       />
