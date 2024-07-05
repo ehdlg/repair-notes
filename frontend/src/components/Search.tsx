dbb7551c0f4a6cb9cc58d83613be641f93488e66
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-05 09:25:32 +0200

    feat(component): clear input value after submit in Search component

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -8,6 +8,8 @@ function Search() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onSubmit = (e: FormEvent) => {
+    if (!inputRef.current) return;
+
     e.preventDefault();
 
     const inputValue = inputRef.current?.value;
@@ -16,22 +18,24 @@ function Search() {
       return toast.error('El ID de la nota no es válido, debe ser un número mayor a 0');
     }
 
-    return navigate(`/${inputValue}`);
+    inputRef.current.value = '';
+
+    navigate(`/${inputValue}`);
   };
 
   return (
     <form
       onSubmit={onSubmit}
-      className='flex justify-center bg-gray-50 p-1 rounded focus-within:border-gray-300 focus-within:border-b-2 border w-3/4 mx-auto'
+      className="flex justify-center bg-gray-50 p-1 rounded focus-within:border-gray-300 focus-within:border-b-2 border w-3/4 mx-auto"
     >
       <input
-        type='number'
+        type="number"
         ref={inputRef}
-        className='[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-nonebg-inherit rounded  outline-none bg-inherit text-center w-full text-2xl text-gray-700'
+        className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-nonebg-inherit rounded  outline-none bg-inherit text-center w-full text-2xl text-gray-700"
       />
 
-      <button type='submit'>
-        <SearchIcon className='text-gray-700 size-6' />
+      <button type="submit">
+        <SearchIcon className="text-gray-700 size-6" />
       </button>
     </form>
   );
