f2a6bba01bd9f2a26658e778bdba7955e4a34ec3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-08 06:36:41 +0200

    fix: remove dark mode styles

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Search from './Search';
 
 function Navbar() {
   return (
-    <aside className='row-span-5 bg-white border-r border-gray-200 dark:bg-slate-900 h-full shadow-[-16px_0px_28px_20px_#edf2f7]'>
+    <aside className='row-span-5 bg-white border-r border-gray-200 h-full shadow-[-16px_0px_28px_20px_#edf2f7]'>
       <nav className='flex flex-col items-start p-6 gap-10 sticky top-0 '>
         <Link to={'/'}>
           <img src='logo.png' alt='Logo Talleres Electromecanicos Lupiañez' />
