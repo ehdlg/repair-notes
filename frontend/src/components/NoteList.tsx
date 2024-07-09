6fa3d5edc2d69684e87eb15be194d1ca3b632f0e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-09 12:26:15 +0200

    feat(component): implement pagination in NoteList component

--- frontend/src/components/NoteList.tsx
+++ frontend/src/components/NoteList.tsx
@@ -1,25 +1,44 @@
+import { useState } from 'react';
 import Note from './Note';
 import useNotes from '../hooks/useNotes';
 import Loading from './Loading';
+import Pagination from './Pagination';
+import { NOTE_LIMIT } from '../constants';
 import { RepairNoteType } from '../types';
+import { calculatePagination } from '../utils';
 
 function NoteList() {
-  const { data, error, isLoading } = useNotes<{count:number, rows: RepairNoteType[]}>('/?');
+  const [page, setPage] = useState(1);
+  const updatePage = {
+    next: () => setPage((prevPage) => prevPage + 1),
+    prior: () => setPage((prevPage) => prevPage - 1),
+  };
+
+  const { limit, offset } = calculatePagination(page, NOTE_LIMIT);
+
+  console.log(limit, offset);
+
+  const { data, error, isLoading } = useNotes<{ count: number; rows: RepairNoteType[] }>(
+    `/?limit=${limit}&offset=${offset}`
+  );
 
   if (isLoading) return <Loading />;
 
-  if (error) return null;
-  
-  const notes = data?.rows
+  if (error || undefined == data) return null;
+
+  const notes = data.rows;
+  const noteCount = data.count;
+  const pageCount = Math.ceil(noteCount / NOTE_LIMIT);
 
   return (
     <>
       <h2 className='mb-8 text-slate-800 text-3xl'>Notas de reparación</h2>
-      <div className='grid grid-cols-5 grid-rows-4 gap-4 justify-center'>
+      <div className='grid grid-cols-5  gap-4 justify-center'>
         {notes?.map((note) => {
           return <Note note={note} />;
         })}
       </div>
+      <Pagination page={page} pageCount={pageCount} update={updatePage} />
     </>
   );
 }
