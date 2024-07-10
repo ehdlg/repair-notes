8efd7306e58349eb87bc8af8dfd820e063815f27
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-10 10:17:39 +0200

    feat(constants): add default condition, where condition dictionary and default where condition constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -4,6 +4,8 @@ export const DEFAULT_LIMIT = 20;
 
 export const DEFAULT_OFFSET = 0;
 
+export const DEFAULT_CONDITION = 'all';
+
 const LIMIT_DATE = new Date().setMonth(new Date().getMonth() - 3);
 
 export const PENDING_FILTER = {
@@ -14,3 +16,17 @@ export const PENDING_FILTER = {
     [Op.is]: null,
   },
 };
+
+export const WHERE_CONDITION = {
+  all: {},
+  repaired: { isRepaired: true },
+  'not-repaired': { isRepaired: false },
+  pending: {
+    entryDate: { [Op.lte]: LIMIT_DATE },
+    departureDate: {
+      [Op.is]: null,
+    },
+  },
+};
+
+export const DEFAULT_WHERE_CONDITION = WHERE_CONDITION[DEFAULT_CONDITION];
