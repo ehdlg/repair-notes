b2593666fd834f8136020236a6c987db9443a8e6
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-19 11:49:35 +0200

    feat(assets): move logo to assets folder, upate logo import in Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,22 @@
 import Search from './Search';
 import { PlusCircleIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 function Navbar() {
   return (
-    <div className='sticky top-0 h-12 bg-white border-b border-gray-200 p-2 opacity-90 shadow-sm'>
-      <nav className='flex justify-around items-center h-full mx-auto'>
+    <div className="sticky top-0 h-12 bg-white border-b border-gray-200 p-2 opacity-90 shadow-sm">
+      <nav className="flex justify-around items-center h-full mx-auto">
         <Link to={'/'}>
           <img
-            src='logo.png'
-            alt='Logo Talleres Electromecanicos Lupiañez'
-            className='h-4 object-contain'
+            src={logo}
+            alt="Logo Talleres Electromecanicos Lupiañez"
+            className="h-4 object-contain"
           />
         </Link>
         <Search />
-        <Link to='/nueva'>
-          <PlusCircleIcon className='size-8 text-gray-700 hover:bg-gray-700 hover:text-white rounded-full transition ease-in' />
+        <Link to="/nueva">
+          <PlusCircleIcon className="size-8 text-gray-700 hover:bg-gray-700 hover:text-white rounded-full transition ease-in" />
         </Link>
       </nav>
     </div>
