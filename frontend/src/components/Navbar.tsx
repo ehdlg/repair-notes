dbe68bc67c6fe859067b84eeb9b8a94a5132ccc6
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-22 09:33:21 +0200

    feat(components): add width to logo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,18 +5,14 @@ import logo from '../assets/logo.png';
 
 function Navbar() {
   return (
-    <div className="sticky top-0 h-12 bg-white border-b border-gray-200 p-2 opacity-90 shadow-sm">
-      <nav className="flex justify-around items-center h-full mx-auto">
+    <div className='sticky top-0 h-12 bg-white border-b border-gray-200 p-2 opacity-90 shadow-sm'>
+      <nav className='flex justify-around items-center h-full mx-auto'>
         <Link to={'/'}>
-          <img
-            src={logo}
-            alt="Logo Talleres Electromecanicos Lupiañez"
-            className="h-4 object-contain"
-          />
+          <img src={logo} alt='Logo Talleres Electromecanicos Lupiañez' className='h-4 w-fit ' />
         </Link>
         <Search />
-        <Link to="/nueva">
-          <PlusCircleIcon className="size-8 text-gray-700 hover:bg-gray-700 hover:text-white rounded-full transition ease-in" />
+        <Link to='/nueva'>
+          <PlusCircleIcon className='size-8 text-gray-700 hover:bg-gray-700 hover:text-white rounded-full transition ease-in' />
         </Link>
       </nav>
     </div>
