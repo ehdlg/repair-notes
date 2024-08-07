9ca5db991f875f84aebf3dd4a9f7b534938f9906
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-07 17:32:09 +0200

    feat(component): update PDF styling to generate 2 delivery notes in 1 document

--- frontend/src/components/PDFDocument.tsx
+++ frontend/src/components/PDFDocument.tsx
@@ -6,11 +6,12 @@ const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
     padding: 40,
+    maxHeight: '100vh',
+    display: 'flex',
   },
   view: {
     display: 'flex',
     alignItems: 'center',
-    marginBottom: 20,
   },
   template: {
     width: '80%',
@@ -20,10 +21,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     height: '50vh',
-    marginTop: '50px',
   },
   data: {
-    width: '48%',
+    width: '50%',
     padding: '20px',
     border: '2px solid black',
     display: 'flex',
@@ -60,7 +60,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     fontSize: '50px',
     fontWeight: 'bold',
-    margin: '50px',
+    marginBottom: '20px',
   },
   warning: {
     border: '2px solid black',
@@ -70,65 +70,158 @@ const styles = StyleSheet.create({
   warningTitle: {
     fontSize: '30px',
   },
-  value: {},
+  machinesContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    border: '1px solid black',
+    padding: '10px',
+    flexGrow: 1,
+  },
+  machinesTitle: {
+    fontSize: '30px',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  machineData: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    alignContent: 'center',
+    justifyContent: 'space-around',
+    fontSize: '20px',
+  },
+  machineText: {
+    borderBottom: '1px solid gray',
+    width: '100%',
+  },
 });
 
 function PDFDocument({ note }: { note: Partial<RepairNoteType> }) {
   return (
     <Document>
-      <Page size='A1' orientation='landscape' style={styles.page}>
-        <View style={styles.view}>
-          <Image src={template} style={styles.template} />
-        </View>
-        <View style={styles.idContainer}>
-          <Text>Nota de reparación No.{note.id}</Text>
-        </View>
-        <View style={styles.dataContainer}>
-          <View style={styles.data}>
-            <View style={styles.row}>
-              <Text style={styles.label}>Fecha de Entrada:</Text>
-              <Text style={styles.value}>
-                {new Date(note?.entryDate as Date).toLocaleDateString('es-ES')}
-              </Text>
+      <Page size='A1' orientation='portrait' style={styles.page}>
+        <View>
+          <View style={styles.view}>
+            <Image src={template} style={styles.template} />
+          </View>
+          <View style={styles.idContainer}>
+            <Text>Nota de reparación No.{note.id}</Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <View style={styles.data}>
+              <View style={styles.row}>
+                <Text style={styles.label}>Fecha de Entrada:</Text>
+                <Text>{new Date(note?.entryDate as Date).toLocaleDateString('es-ES')}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Cliente:</Text>
+                <Text>{note.client}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Número de Teléfono:</Text>
+                <Text>{note.phoneNumber}</Text>
+              </View>
+              <Text style={styles.machinesTitle}>Máquinas</Text>
+              <View style={styles.machinesContainer}>
+                <View style={styles.machineData}>
+                  {note.model?.split('|').map((machine) => {
+                    return (
+                      <View style={styles.machineText}>
+                        <Text>{machine}</Text>
+                      </View>
+                    );
+                  })}
+                </View>
+                <View style={styles.machineData}>
+                  {note.malfunction?.split('|').map((malfunction) => {
+                    return (
+                      <View style={styles.machineText}>
+                        <Text>{malfunction}</Text>
+                      </View>
+                    );
+                  })}
+                </View>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Reparación garantía:</Text>
+                <Text>{note.garanty ? 'Sí' : 'No'}</Text>
+              </View>
+              <View style={styles.warning}>
+                <Text style={styles.warningTitle}>Nota de interés para el cliente</Text>
+                <Text>
+                  * Para la recogida de la maquinaria es imprescindible la entrega de este resguardo
+                </Text>
+                <Text>
+                  * Los presupuestos de reparación que no sean aceptados se les sobrará el valor del
+                  tiempo aplicado a la elaboración de dicho presupuesto
+                </Text>
+                <Text>
+                  * Se prevee un plazo de 90 días desde la fecha de admisión de la máquina para ser
+                  retirada, de lo contrario se entenderá abandonada autorizando su desguace
+                </Text>
+              </View>
             </View>
-            <View style={styles.row}>
-              <Text style={styles.label}>Cliente:</Text>
-              <Text style={styles.value}>{note.client}</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.label}>Número de Teléfono:</Text>
-              <Text style={styles.value}>{note.phoneNumber}</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.label}>Máquina(s)/Modelo(s):</Text>
-              <Text style={styles.value}>{note.model?.split('|').join(', ')}</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.label}>Avería:</Text>
-              <Text style={styles.value}>{note.malfunction?.split('|').join(', ')}</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.label}>Reparación garantía:</Text>
-              <Text style={styles.value}>{note.garanty ? 'Sí' : 'No'}</Text>
-            </View>
-            <View style={styles.warning}>
-              <Text style={styles.warningTitle}>Nota de interés para el cliente</Text>
-              <Text>
-                * Para la recogida de la maquinaria es imprescindible la entrega de este resguardo
-              </Text>
-              <Text>
-                * Los presupuestos de reparación que no sean aceptados se les sobrará el valor del
-                tiempo aplicado a la elaboración de dicho presupuesto
-              </Text>
-              <Text>
-                * Se prevee un plazo de 90 días desde la fecha de admisión de la máquina para ser
-                retirada, de lo contrario se entenderá abandonada autorizando su desguace
-              </Text>
+            <View style={styles.emptyColumn}>
+              <Text style={styles.label}>MATERIALES EMPLEADOS</Text>
+              <Text>MANO DE OBRA ____________________ HORAS</Text>
             </View>
           </View>
-          <View style={styles.emptyColumn}>
-            <Text style={styles.label}>MATERIALES EMPLEADOS</Text>
-            <Text>MANO DE OBRA ____________________ HORAS</Text>
+        </View>
+        <View>
+          <View style={styles.view}>
+            <Image src={template} style={styles.template} />
+          </View>
+          <View style={styles.idContainer}>
+            <Text>Nota de reparación No.{note.id}</Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <View style={styles.data}>
+              <View style={styles.row}>
+                <Text style={styles.label}>Fecha de Entrada:</Text>
+                <Text>{new Date(note?.entryDate as Date).toLocaleDateString('es-ES')}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Cliente:</Text>
+                <Text>{note.client}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Número de Teléfono:</Text>
+                <Text>{note.phoneNumber}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Máquina(s)/Modelo(s):</Text>
+                <Text>{note.model?.split('|').join(', ')}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Avería:</Text>
+                <Text>{note.malfunction?.split('|').join(', ')}</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.label}>Reparación garantía:</Text>
+                <Text>{note.garanty ? 'Sí' : 'No'}</Text>
+              </View>
+              <View style={styles.warning}>
+                <Text style={styles.warningTitle}>Nota de interés para el cliente</Text>
+                <Text>
+                  * Para la recogida de la maquinaria es imprescindible la entrega de este resguardo
+                </Text>
+                <Text>
+                  * Los presupuestos de reparación que no sean aceptados se les sobrará el valor del
+                  tiempo aplicado a la elaboración de dicho presupuesto
+                </Text>
+                <Text>
+                  * Se prevee un plazo de 90 días desde la fecha de admisión de la máquina para ser
+                  retirada, de lo contrario se entenderá abandonada autorizando su desguace
+                </Text>
+              </View>
+            </View>
+            <View style={styles.emptyColumn}>
+              <Text style={styles.label}>MATERIALES EMPLEADOS</Text>
+              <Text>MANO DE OBRA ____________________ HORAS</Text>
+            </View>
           </View>
         </View>
       </Page>
